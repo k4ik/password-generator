2c528d6cd812dffdeb3bd6c9005fbb19fa6aa62e
k4ik <kaikleal1234@gmail.com>
2024-02-12 11:30:48 -0300

    feat: Added password length validation

--- password.php
+++ password.php
@@ -3,8 +3,14 @@
         $chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOQPRSTUVWXYZ!@#$%^&*()_-+=";
         $password = "";
 
-        for($i = 0; $i < $lenght; $i++) {
-            $password .= $chars[rand(0, strlen($chars) - 1)];
+        if ($lenght < 8) {
+            echo '<script>alert("Password must be longer than 8 characters.");</script>';
+        } elseif($lenght > 128) {
+            echo '<script>alert("The maximum password length is 128 characters.");</script>';
+        } else {
+            for($i = 0; $i < $lenght; $i++) {
+                $password .= $chars[rand(0, strlen($chars) - 1)];
+            }
         }
 
         return $password;
