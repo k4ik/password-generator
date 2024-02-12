14e5e934086f171c0588926aa8f7fe1acc16b636
k4ik <kaikleal1234@gmail.com>
2024-02-12 11:49:34 -0300

    fix: deploy

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Password Generator</title>
+    <link rel="shortcut icon" href="assets/images/favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" href="assets/styles/style.css">
+</head>
+<body>
+    <main>
+        <div class="container">
+            <h1>Password Generator</h1>
+            <form method="post">
+                <label for="lenght">Password length:</label>
+                <input type="text" placeholder="12" name="lenght">
+            </form>
+            <p>
+                <?php 
+                    include 'password.php';
+
+                    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                        $lenght = $_POST["lenght"];
+
+                        $randomPassword = generatePassword($lenght);
+                        echo $randomPassword; 
+                    }
+                ?>
+            </p>
+        </div>
+    </main>
+</body>
+</html>
