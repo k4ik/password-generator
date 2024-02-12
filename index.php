5d1822556fa75bc252bed62cac7e844631aee396
k4ik <kaikleal1234@gmail.com>
2024-02-12 10:35:54 -0300

    feat: create form html

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php 
+    include 'password.php';
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $lenght = $_POST["lenght"];
+
+        $randomPassword = generatePassword($lenght);
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Password Generator</title>
+    <link rel="stylesheet" href="assets/styles/style.css">
+</head>
+<body>
+    <main>
+        <h1>Password Generator</h1>
+        <form method="post">
+            <label for="lenght">Password length:</label>
+            <input type="text" placeholder="12" name="lenght">
+        </form>
+        <p><?php echo $randomPassword; ?></p>
+    </main>
+</body>
+</html>
