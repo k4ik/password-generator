b8f1fd73062b6f095a1ea8ea281215ef8138d3c5
k4ik <kaikleal1234@gmail.com>
2024-02-12 11:43:17 -0300

    fix: deploy

--- api/index.php
+++ api/index.php
@@ -1,12 +1,4 @@
-<?php 
-    include 'password.php';
-
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        $lenght = $_POST["lenght"];
-
-        $randomPassword = generatePassword($lenght);
-    }
-?>
+>
 
 <!DOCTYPE html>
 <html lang="pt-br">
@@ -15,7 +7,71 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Password Generator</title>
     <link rel="shortcut icon" href="assets/images/favicon.ico" type="image/x-icon">
-    <link rel="stylesheet" href="assets/styles/style.css">
+    <style>
+        * {
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+        }
+
+        :root {
+            --bg-color: #2C3E50;
+            --container-bg-color: #ECF0F1;
+            --text-color: #323f3f;
+        }
+
+        main {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            height: 100vh;
+            background-color: var(--bg-color);
+        }
+
+        .container {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            padding: 30px 0;
+
+            max-width: 500px;
+            width: 100%;
+
+            margin: 0 auto;
+
+            background-color: var(--container-bg-color);
+        }
+
+        form {
+            display: flex;
+            flex-direction: column;
+            gap: 5px;
+            width: 80%;
+        }
+
+        p, label, h1 {
+            color: var(--text-color);
+            width: 100%;
+        }
+
+        h1 {
+            margin-bottom: 20px;
+            text-align: center;
+
+        }
+
+        input {
+            outline: none;
+            border: 1px solid var(--text-color);
+            padding: 2px 5px;
+        }
+
+        p {
+            margin-top: 20px;
+            text-align: center;
+        }
+    </style>
 </head>
 <body>
     <main>
@@ -25,7 +81,18 @@
                 <label for="lenght">Password length:</label>
                 <input type="text" placeholder="12" name="lenght">
             </form>
-            <p><?php echo $randomPassword; ?></p>
+            <p>
+                <?php 
+                    include 'password.php';
+
+                    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                        $lenght = $_POST["lenght"];
+
+                        $randomPassword = generatePassword($lenght);
+                        echo $randomPassword; 
+                    }
+                ?>
+            </p>
         </div>
     </main>
 </body>
