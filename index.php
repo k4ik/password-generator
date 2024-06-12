68f1573f7f9b4a081ea76f90cd3e2fafbdd9b03b
k4ik <kaikleal1234@gmail.com>
2024-06-12 14:08:15 -0300

    fix: index

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,27 +8,43 @@
     <link rel="shortcut icon" href="assets/images/favicon.ico" type="image/x-icon">
     <link rel="stylesheet" href="assets/styles/style.css">
 </head>
+
 <body>
     <main>
         <div class="container">
             <h1>Password Generator</h1>
             <form method="post">
-                <label for="lenght">Password length:</label>
-                <input type="text" placeholder="12" name="lenght">
+                <label for="length">Password length:</label>
+                <input type="text" placeholder="12" name="length" id="length">
+                <button type="submit">Submit</button>
             </form>
             <p>
-                <?php 
-                    include 'password.php';
+                <?php
+                function generatePassword($length)
+                {
+                    $chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOQPRSTUVWXYZ!@#$%^&*()_-+=";
+                    $password = "";
 
-                    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-                        $lenght = $_POST["lenght"];
-
-                        $randomPassword = generatePassword($lenght);
-                        echo $randomPassword; 
+                    if ($length < 8) {
+                        return "Password must be longer than 8 characters.";
+                    } elseif ($length > 32) {
+                        return "The maximum password length is 32 characters.";
+                    } else {
+                        for ($i = 0; $i < $length; $i++) {
+                            $password .= $chars[rand(0, strlen($chars) - 1)];
+                        }
+                        return $password;
                     }
+                }
+
+                if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                    $length = $_POST["length"];
+                    echo generatePassword($length);
+                }
                 ?>
             </p>
         </div>
     </main>
 </body>
+
 </html>
